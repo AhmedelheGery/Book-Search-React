d099a890c696c367d13d6a2317156aa02f2b33d4
Ahmed Elhegery <ahmed.3laa.2020@gmail.com>
2019-09-24 13:31:03 +0200

    Updates

---
+++ precache-manifest.bf23ab664464ef93079ebd16b5b14137.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c018802c7b15f89e9c2a643018679a1e",
+    "url": "/Book-Search-React/index.html"
+  },
+  {
+    "revision": "6d56d825877b56749bda",
+    "url": "/Book-Search-React/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ea942a1465403e0f2bf7",
+    "url": "/Book-Search-React/static/css/main.5133d260.chunk.css"
+  },
+  {
+    "revision": "6d56d825877b56749bda",
+    "url": "/Book-Search-React/static/js/2.eab5ffe5.chunk.js"
+  },
+  {
+    "revision": "ea942a1465403e0f2bf7",
+    "url": "/Book-Search-React/static/js/main.f42b7717.chunk.js"
+  },
+  {
+    "revision": "d3c7faf1b537e86ea01e",
+    "url": "/Book-Search-React/static/js/runtime~main.9291f792.js"
+  },
+  {
+    "revision": "4794d6b5cc39488e04ec044754d9cdca",
+    "url": "/Book-Search-React/static/media/searchBG1.4794d6b5.jpg"
+  }
+]);
